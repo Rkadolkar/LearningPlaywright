85ce3949472360b9edd0965f5c20e55f57bc1d51
Catch <catch@example.com>
2026-02-28 08:34:12 +0530

    Update code

--- Chapter_07_loops/increment_task.js
+++ Chapter_07_loops/increment_task.js
@@ -7,7 +7,9 @@ let b = a++ + ++a
 //a++=5 + 6+1=12 
 //value of a after b cal is 7 (last calculation of a is 7)
 
-let c = a++ + ++a + ++a + ++a + a++
+//let c = a++ + ++a + ++a + ++a + a++
+let c = ++a + ++a + ++a + ++a + a++
+
 //c=7+(8+1)+(9+1)+(10+1)+11=48
 //value of a after c cal is 12 (last calculation of a is 11+1=12)
 console.log(a)
